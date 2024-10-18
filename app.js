517e3bec725558f6c9aeebd4557685789442766c
Shubham-Sharad-Mane <shubhammane9322@gmail.com>
2024-10-18 10:36:51 +0530

    updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function wishMe(){
     var hour = day.getHours();
 
     if(hour>=0 && hour<12){
-        speak("Good Morning Boss...")
+        speak("Good Morning Sir...")
     }
 
     else if(hour>12 && hour<17){
